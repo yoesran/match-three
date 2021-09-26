a13245c1077df4847bc08e4dcd2e7513433857b0
yoesran <19523170naufal@gmail.com>
2021-09-26 21:54:02 +0800

    first

---
+++ Assets/Scripts/TimerManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerManager : MonoBehaviour
+{
+    #region Singleton
+
+    private static TimerManager _instance = null;
+
+    public static TimerManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<TimerManager>();
+
+                if (_instance == null)
+                {
+                    Debug.LogError("Fatal Error: TimeManager not Found");
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public int duration;
+
+    private float time;
+
+    private void Start()
+    {
+        time = 0;
+    }
+
+    private void Update()
+    {
+        if (GameFlowManager.Instance.IsGameOver)
+        {
+            return;
+        }
+
+        if (time > duration)
+        {
+            GameFlowManager.Instance.GameOver();
+            return;
+        }
+
+        time += Time.deltaTime;
+    }
+
+    // Fitur tambah waktu saat bertambah score
+    public void AddTime()
+    {
+        duration += 10;
+    }
+
+    public float GetRemainingTime()
+    {
+        return duration - time;
+    }
+}
---
+++ Assets/Scripts/UITime.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITime : MonoBehaviour
+{
+    public Text time;
+
+    private void Update()
+    {
+        time.text = GetTimeString(TimerManager.Instance.GetRemainingTime() + 1);
+    }
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private string GetTimeString(float timeRemaining)
+    {
+        int minute = Mathf.FloorToInt(timeRemaining / 60);
+        int second = Mathf.FloorToInt(timeRemaining % 60);
+
+        return string.Format("{0} : {1}", minute.ToString(), second.ToString());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+}
